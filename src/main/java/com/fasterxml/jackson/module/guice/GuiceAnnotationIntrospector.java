8bcb2a0f09fd7fe695a762b630162dc0b5fd2577
cvuijst <cvuijst@hotmail.com>
2014-01-07 10:48:35 +0100

    Added explicit check on javax.inject.Named annotation

--- src/main/java/com/fasterxml/jackson/module/guice/GuiceAnnotationIntrospector.java
+++ src/main/java/com/fasterxml/jackson/module/guice/GuiceAnnotationIntrospector.java
@@ -1,34 +1,44 @@
 package com.fasterxml.jackson.module.guice;
 
+import java.lang.annotation.Annotation;
+
+import javax.inject.Named;
+
 import com.fasterxml.jackson.annotation.JacksonInject;
 import com.fasterxml.jackson.databind.introspect.AnnotatedMember;
 import com.fasterxml.jackson.databind.introspect.NopAnnotationIntrospector;
 import com.google.inject.BindingAnnotation;
 import com.google.inject.Key;
 
-import java.lang.annotation.Annotation;
+public class GuiceAnnotationIntrospector extends NopAnnotationIntrospector {
+	private static final long serialVersionUID = 1L;
 
-public class GuiceAnnotationIntrospector extends NopAnnotationIntrospector
-{
-    private static final long serialVersionUID = 1L;
+	@Override
+	public Object findInjectableValueId(final AnnotatedMember m) {
+		// Is this needed?
+		if (m.getAnnotation(JacksonInject.class) == null) {
+			return null;
+		}
+		Annotation guiceAnnotation = null;
+		for (Annotation annotation : m.annotations()) {
 
-    @Override
-    public Object findInjectableValueId(AnnotatedMember m)
-    {
-        // Is this needed?
-        if (m.getAnnotation(JacksonInject.class) == null) {
-            return null;
-        }
-        Annotation guiceAnnotation = null;
-        for (Annotation annotation : m.annotations()) {
-            if (annotation.annotationType().isAnnotationPresent(BindingAnnotation.class)) {
-                guiceAnnotation = annotation;
-                break;
-            }
-        }
-        if (guiceAnnotation == null) {
-            return Key.get(m.getGenericType());
-        }
-        return Key.get(m.getGenericType(), guiceAnnotation);
-    }
+			// Explicit check on javax.inject.Named annotation
+			if (annotation.annotationType()
+					.equals(Named.class)) {
+				guiceAnnotation = annotation;
+				break;
+			}
+
+			// Binding annotation (Google inject based)
+			if (annotation.annotationType()
+					.isAnnotationPresent(BindingAnnotation.class)) {
+				guiceAnnotation = annotation;
+				break;
+			}
+		}
+		if (guiceAnnotation == null) {
+			return Key.get(m.getGenericType());
+		}
+		return Key.get(m.getGenericType(), guiceAnnotation);
+	}
 }
